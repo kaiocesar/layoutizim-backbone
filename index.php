98ed0000ec3fae85ff39d55da398b59cefd48104
Desenvolvimento IronSystems <desenvolvimento@ironsystems.com.br>
2015-07-13 17:59:45 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Brincando com Backbone</title>
+</head>
+<body>
+
+
+
+
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="http://documentcloud.github.io/underscore/underscore-min.js"></script>
+	<script type="text/javascript" src="http://backbonejs.org/backbone-min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(function(){
+
+	var HelloView = Backbone.View.extend({
+		
+		initialize: function(){
+			this.render();
+		},
+		render: function(){
+			this.$el.append("<h1>Hi, im backbone.</h1>");
+		}
+	});
+
+
+	var helloView = new HelloView({el: $('body')});
+
+});
